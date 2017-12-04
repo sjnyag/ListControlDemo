c7c6fcf3c3f613917872a477d536ef9674fbcecf
sjnyag <sjnyas@gmail.com>
2017-12-04 19:08:26 +0900

    initial commit

---
+++ app/src/androidTest/java/com/sjn/demo/listcontrolldemo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.sjn.demo.listcontrolldemo
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.sjn.demo.listcontrolldemo", appContext.packageName)
+    }
+}
